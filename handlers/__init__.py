e25d7536a070e2277de576c069acef383c086bed
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2024-10-20 22:34:54 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+from .start_handler import start_handler
+from .game_handler import game_handler, join_handler, players_handler
+from .admin_handler import force_start_handler
+
+__all__ = ["start_handler", "game_handler", "join_handler", "players_handler", "force_start_handler"]
