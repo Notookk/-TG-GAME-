8900cc41314b28241fb8fcd2ec490039878040cf
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2024-10-20 22:21:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
-from telegram.ext import (
-    Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from utils.config import BOT_TOKEN
+from handlers.game_handler import (
+    start_handler, game_handler, forcestart_handler, category_selection_handler
 )
-from handlers import start_handler, game_handler, join_handler, admin_handler, error_handler
+from handlers.player_handler import players_handler
 
 def main():
-    updater = Updater(token="YOUR_BOT_TOKEN", use_context=True)
-    dispatcher = updater.dispatcher
+    updater = Updater(BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start_handler.start_handler))
-    dispatcher.add_handler(CommandHandler("game", game_handler.game_handler))
-    dispatcher.add_handler(CommandHandler("forcestart", admin_handler.force_start_handler))
-    dispatcher.add_handler(CallbackQueryHandler(game_handler.join_game_handler, pattern="join"))
-    dispatcher.add_error_handler(error_handler.error_handler)
+    # Register handlers
+    dp.add_handler(CommandHandler("start", start_handler))
+    dp.add_handler(CommandHandler("game", game_handler))
+    dp.add_handler(CommandHandler("forcestart", forcestart_handler))
+    dp.add_handler(CommandHandler("players", players_handler))
+    dp.add_handler(CallbackQueryHandler(category_selection_handler, pattern="^category_"))
 
+    # Start the bot
     updater.start_polling()
     updater.idle()
 
